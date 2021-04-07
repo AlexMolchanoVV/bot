c9436975baffb85e8c34e121fe008ff7e78f3720
Alexandr <abc@mail.ru>
2021-04-07 21:33:40 +0300

    avs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tegbot',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='inbo-02-19',
+    author_email='asd',
+    description='bot'
+)
